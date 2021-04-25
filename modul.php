29504999a83590bdb957ff15c8be8756094076e0
Exaudi01 <83176896+Exaudi01@users.noreply.github.com>
2021-04-25 22:59:52 +0700

    Add files via upload

---
+++ modul.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="style.css" rel="stylesheet">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+.navbar {
+  width: 100%;
+  background-color: rgba(227, 241, 18, 0.938);
+  overflow: auto;
+}
+
+.navbar a {
+  float: left;
+  padding: 12px;
+  color: rgb(17, 16, 16);
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.navbar a:hover {
+  background-color: rgb(31, 128, 219);
+}
+
+.active {
+  background-color: #e5e912;
+}
+
+
+</style>
+<body>
+<div class="navbar">
+  <a class="active" href="#"><i class="fa fa-fw fa-university"></i> E-campus </a> 
+  <a href="home.php"><i class="fa fa-fw fa-home"></i> Home</a> 
+  <a href="modul.php"><i class="fa fa-fw fa-book"></i> Modul</a> 
+  <a href="data_mahasiswa_akademis.php"><i class="fa fa-fw fa-graduation-cap"></i> Data Mahasiswa</a>
+  <a href="#"><i class="fa fa-fw fa-user"></i> Data Akademik</a> 
+  <a href="index.php"><i class="fa fa-fw fa-sign-out"></i> Logout</a>
+</div>
+
+
+
+<hr />
+<br><br><br><br><br><br>
+
+<div class="text1">
+<h2>Week 1</h2><br><br>
+<hr />
+<h2>Week 2</h2><br><br>
+<hr />
+<h2>Week 3</h2><br><br>
+<hr />
+<h2>Week 4</h2><br><br>
+<hr />
+</div>
+
+</body>
+
+</html>
